87dd044162a10b96ab8e8d709378ff9751bfe75a
David Wolever <wolever@sheertex.com>
2022-05-07 18:59:36 -0400

    2.0.0-alpha.5

--- Casks/upnext.rb
+++ Casks/upnext.rb
@@ -1,9 +1,9 @@
 # genreated by upnext-macos/relaese:build_homebrew_cask()
 cask 'upnext' do
-  version '2.0.0-alpha.4'
-  sha256 '3f76ea20a1c86e9699be38c375992d6e9e1971d15cf9c9cbccc7b23c8f489c0f'
+  version '2.0.0-alpha.5'
+  sha256 'f8b5ada56baec19b00380f42b193e39f68580d3eaea0d6f6221f0b80fc36e481'
 
-  url "https://upnextapp.netlify.app/versions/UpNext-2.0.0-alpha.4.zip"
+  url "https://upnextapp.netlify.app/versions/UpNext-2.0.0-alpha.5.zip"
   appcast "https://upnextapp.netlify.app/appcast.xml"
   name 'Up Next'
   homepage 'https://upnextapp.com/'
